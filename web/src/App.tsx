7a153074e80c9007225611d88ee5727aeca83a70
hhhello <hhhello0507@dgsw.hs.kr>
2024-08-26 10:38:51 +0900

    Fix

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,27 +1,28 @@
-import React, { useEffect } from "react";
-import { GlobalStyle } from "@style/globalStyle";
+import React, {useEffect} from "react";
+import {GlobalStyle} from "@style/globalStyle";
 import AppStateProvider from "@provider/theme/AppStateProvider";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import Routes from "@src/routes";
 import axios from "axios";
 
 function App() {
-  useEffect(() => {
-    axios
-      .get(`${process.env.SERVER_BASE_URL ?? "http://localhost:8080"}/test`)
-      .then((res) => {
-        console.log(res.data);
-      });
-  }, []);
+    useEffect(() => {
+        axios
+            .get(`${process.env.SERVER_BASE_URL ?? "http://localhost:8080"}/test`)
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch(e => console.log(e));
+    }, []);
 
-  return (
-    <AppStateProvider>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Routes />
-      </BrowserRouter>
-    </AppStateProvider>
-  );
+    return (
+        <AppStateProvider>
+            <BrowserRouter>
+                <GlobalStyle/>
+                <Routes/>
+            </BrowserRouter>
+        </AppStateProvider>
+    );
 }
 
 export default App;
