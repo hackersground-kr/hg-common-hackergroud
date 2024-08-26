0a7ede9914aa6b8fa44bf0ccbc7046925c0ea24b
hhhello <hhhello0507@dgsw.hs.kr>
2024-08-26 10:36:29 +0900

    Create Network layer

---
+++ web/src/data/Repository.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import config from "@src/config";
+
+const usAxios = axios.create({
+    baseURL: config.baseUrl,
+})
+
+class Repository {
+    async ai1(prompt: string): Promise<Response> {
+        const {data} = await usAxios.post('ai/1', {
+            text: prompt
+        });
+        return data;
+    }
+
+    async ai2(prompt: string): Promise<Response> {
+        const {data} = await usAxios.post('ai/2', {
+            text: prompt
+        });
+        return data;
+    }
+
+    async ai3(prompt: string): Promise<Response> {
+        const {data} = await usAxios.post('ai/3', {
+            text: prompt
+        });
+        return data;
+    }
+}
