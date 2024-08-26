f1d72f65b627949c0151d85e161e6aedf4d91ee8
이슬아 <rheeseula@gmail.com>
2024-08-26 17:18:48 +0900

    feat: TestController

---
+++ server/src/main/kotlin/com/server/TestController.java
@@ -0,0 +1,16 @@
+package com.server;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+}
