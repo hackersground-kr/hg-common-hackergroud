250e1e64c64a33f1b232040c17945ea0098c368f
이슬아 <rheeseula@gmail.com>
2024-08-26 10:32:52 +0900

    fix: stashed change

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,27 @@
-import React, {useEffect} from "react";
-import {GlobalStyle} from "@style/globalStyle";
+import React, { useEffect } from "react";
+import { GlobalStyle } from "@style/globalStyle";
 import AppStateProvider from "@provider/theme/AppStateProvider";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Routes from "@src/routes";
 import axios from "axios";
 
 function App() {
-    useEffect(() => {
-        (async () => {
-            try {
-                const data = await axios.get("http://localhost:8080/test");
-                console.log(data.data);
-            } catch (e) {
-                console.log(e);
-            }
-        })()
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`${process.env.SERVER_BASE_URL ?? "http://localhost:8080"}/test`)
+      .then((res) => {
+        console.log(res.data);
+      });
+  }, []);
 
-    return (
-        <AppStateProvider>
-            <BrowserRouter>
-                <GlobalStyle/>
-                <Routes/>
-            </BrowserRouter>
-        </AppStateProvider>
-    );
+  return (
+    <AppStateProvider>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes />
+      </BrowserRouter>
+    </AppStateProvider>
+  );
 }
 
 export default App;
