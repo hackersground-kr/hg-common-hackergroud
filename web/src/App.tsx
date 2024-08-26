9f017a8b61f115cb15dd856d426ea0667a3fe88b
8954sood <8954sood@naver.com>
2024-08-26 19:10:06 +0900

    feat: Preload Images

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {GlobalStyle} from "@style/globalStyle";
 import AppStateProvider from "@provider/theme/AppStateProvider";
 import {BrowserRouter} from "react-router-dom";
 import Routes from "@src/routes";
 
 function App() {
+  const preloadImage = () => {
+    const images = ["bg1.webp", "bg2.png", "bg3.png", "bg4.png", "bg5.png", "bg6.webp", "bg7.png", "bg8.png", "bg9.png", "bg10.png", "bg10.png"
+      , "char1.png", "char2.png", "char3.png", "char4.png", "char5.png", "char6.png", "char7.png", "char8.png", 
+    ]
+    images.forEach((image) => {
+      const img = new Image();
+      img.src = "image/" + image
+    })
+  }
+
+  useEffect(() => {
+    preloadImage()
+  }, [])
+  
     return (
         <AppStateProvider>
             <BrowserRouter>
