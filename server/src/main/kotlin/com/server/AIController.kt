be0b4e282b0a90c1913a85c9b22015e28ca1f213
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-08-26 10:05:16 +0900

    Resetting server for kotlin

---
+++ server/src/main/kotlin/com/server/AIController.kt
@@ -0,0 +1,37 @@
+package com.server
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/ai")
+class AIController(
+    private val aiService: AiService
+) {
+
+    @PostMapping("/1")
+    fun sin1(
+        @RequestBody request: Request
+    ): Any{
+        return aiService.sin1(request.text)
+    }
+
+    @PostMapping("/2")
+    fun sin2(
+        @RequestBody request: Request
+    ): Any{
+        return aiService.sin2(request.text)
+    }
+
+
+    @PostMapping("/3")
+    fun sin3(
+        @RequestBody request: Request
+    ): Any{
+        return aiService.sin3(request.text)
+    }
+
+}
---
+++ server/src/main/kotlin/com/server/Request.kt
@@ -0,0 +1,5 @@
+package com.server
+
+data class Request(
+    val text: String
+)
